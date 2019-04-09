0d1503cfe068e07418b4666455368a96d5b9412c
Yang Liu <liuyang12@focusmedia.cn>
2019-04-09 16:59:48 +0800

    update

--- sdk/test_verify.py
+++ sdk/test_verify.py
@@ -1,5 +1,9 @@
-
-
+import requests
 
 if __name__ == '__main__':
-    endpoint = 'http://47.101.196.204:8080/api/v1/faces/verify'
+    url = 'http://47.101.196.204:8080/api/v1/faces/verify'
+
+    files = {'file1': open('sample_verify_1.jpg', 'rb'), 'file2': open('sample_verify_2.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
