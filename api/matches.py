b84cc0772dc9a588f96c948a9d105881bae4fb8c
Yang Liu <liuyang12@focusmedia.cn>
2019-04-30 16:08:35 +0800

    update

--- api/matches.py
+++ api/matches.py
@@ -1,10 +1,10 @@
 from flask import jsonify
 
-from utils_match import video_match
+from utils_match import match_video
 from . import api
 
 
 @api.route('/matches/recognize', methods=['POST'])
 def recognize():
-    is_match, index, time_in_video, elapsed, fn = video_match()
+    is_match, index, time_in_video, elapsed, fn = match_video()
     return jsonify({'is_match': is_match, 'index': index, 'time_in_video': time_in_video, 'elapsed': elapsed})
