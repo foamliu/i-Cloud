2544e0794f2a83c4ce2635fe2901c9fb90c339eb
Yang Liu <liuyang12@focusmedia.cn>
2019-05-20 11:40:48 +0800

    update

--- utils_tag.py
+++ utils_tag.py
@@ -1,3 +1,5 @@
+import json
+
 import redis
 from flask import request
 
@@ -7,22 +9,25 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 
 
 def search_tag():
+    gender = age = zcdj = yf = yc = intr = None
     mac = str(request.form.get('macAddress'))
     mac = mac.replace(':', '')
     md5 = md5_hash(mac)
     ret = r.get(md5)
-    gender = age = zcdj = yf = yc = intr = None
-    if 'gender' in ret:
-        gender = ret['gender']
-    if 'age' in ret:
-        age = ret['age']
-    if 'zcdj' in ret:
-        zcdj = ret['zcdj']
-    if 'yf' in ret:
-        yf = ret['yf']
-    if 'yc' in ret:
-        yc = ret['yc']
-    if 'intr' in ret:
-        intr = ret['intr']
+    if ret:
+        ret = ret.decode()
+        ret = json.loads(ret)
+        if 'gender' in ret:
+            gender = ret['gender']
+        if 'age' in ret:
+            age = ret['age']
+        if 'zcdj' in ret:
+            zcdj = ret['zcdj']
+        if 'yf' in ret:
+            yf = ret['yf']
+        if 'yc' in ret:
+            yc = ret['yc']
+        if 'intr' in ret:
+            intr = ret['intr']
 
     return gender, age, zcdj, yf, yc, intr
