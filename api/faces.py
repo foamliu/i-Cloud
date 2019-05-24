8b55e93ae2ff5a33d48c27052825d63eaa38bf1e
Yang Liu <liuyang12@focusmedia.cn>
2019-05-24 15:10:39 +0800

    update

--- api/faces.py
+++ api/faces.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 
-from utils_face import face_verify
+from utils_face import face_verify, face_detect, face_search
 from . import api
 
 
@@ -8,3 +8,15 @@ from . import api
 def verify():
     is_same, prob, elapsed, fn_1, fn_2 = face_verify()
     return jsonify({'is_same': is_same, 'prob': prob, 'elapsed': elapsed})
+
+
+@api.route('/faces/detect', methods=['POST'])
+def detect():
+    num_faces, elapsed, fn = face_detect()
+    return jsonify({'num_faces': num_faces, 'elapsed': elapsed})
+
+
+@api.route('/faces/search', methods=['POST'])
+def search():
+    name, prob, file, elapsed = face_search()
+    return jsonify({'name': name, 'prob': prob, 'elapsed': elapsed})
--- sdk/test_faces.py
+++ sdk/test_faces.py
@@ -2,8 +2,22 @@ import requests
 
 if __name__ == '__main__':
     url = 'http://47.101.196.204:8080/api/v1/faces/verify'
+    print(url)
+    files = {'file1': open('test_image_1.jpg', 'rb'), 'file2': open('test_image_2.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
 
-    files = {'file1': open('sample_verify_1.jpg', 'rb'), 'file2': open('sample_verify_2.jpg', 'rb')}
+    url = 'http://47.101.196.204:8080/api/v1/faces/detect'
+    print(url)
+    files = {'file': open('test_image_1.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
+
+    url = 'http://47.101.196.204:8080/api/v1/faces/search'
+    print(url)
+    files = {'file': open('test_image_1.jpg', 'rb')}
     r = requests.post(url, files=files)
     print(r.status_code)
     print(r.text)
