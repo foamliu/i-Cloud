0a6c43801704a2b9099ee60caade9207f15ac2cf
Yang Liu <liuyang12@focusmedia.cn>
2019-05-30 11:52:53 +0800

    update

--- sdk/test_match.py
+++ sdk/test_match.py
@@ -4,7 +4,7 @@ import json
 if __name__ == '__main__':
     url = 'http://47.101.196.204:8080/api/v1/matches/recognize'
 
-    files = {'file': open('9707366-1.jpg', 'rb')}
+    files = {'file': open('test_image_3.jpg', 'rb')}
     r = requests.post(url, files=files)
     print(r.status_code)
     print(r.text)
