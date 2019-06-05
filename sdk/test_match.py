81feacdc896d8464e58e9e0ac2e8a3cd0da95997
Yang Liu <liuyang12@focusmedia.cn>
2019-06-05 11:40:25 +0800

    update

--- sdk/test_match.py
+++ sdk/test_match.py
@@ -1,10 +1,39 @@
-import requests
 import json
+
+import requests
 
 if __name__ == '__main__':
     url = 'http://47.101.196.204:8080/api/v1/matches/recognize'
 
     files = {'file': open('test_image_3.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
+    ret = json.loads(r.text)
+    print(ret['name'])
+
+    files = {'file': open('test_image_4.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
+    ret = json.loads(r.text)
+    print(ret['name'])
+
+    files = {'file': open('test_image_5.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
+    ret = json.loads(r.text)
+    print(ret['name'])
+
+    files = {'file': open('test_image_6.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
+    ret = json.loads(r.text)
+    print(ret['name'])
+
+    files = {'file': open('test_image_7.jpg', 'rb')}
     r = requests.post(url, files=files)
     print(r.status_code)
     print(r.text)
