5a6d7a573241a0c2600ba8371c71f3750e014743
Yang Liu <liuyang12@focusmedia.cn>
2019-06-17 10:33:18 +0800

    update

--- models.py
+++ models.py
@@ -290,3 +290,24 @@ class ArcMarginModel(nn.Module):
         output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
         output *= self.s
         return output
+
+
+from torchvision import models
+
+
+class FaceAttributeModel(nn.Module):
+    def __init__(self):
+        super(FaceAttributeModel, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        # Remove linear and pool layers (since we're not doing classification)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.fc = nn.Linear(2048, 5)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, images):
+        x = self.resnet(images)  # [N, 2048, 1, 1]
+        x = x.view(-1, 2048)  # [N, 2048]
+        x = self.fc(x)
+        x = self.sigmoid(x)  # [N, 8]
+        return x
