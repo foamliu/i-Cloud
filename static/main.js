72ffe88f15653f35c19a798a822119e393568ecc
Yang Liu <liuyang12@focusmedia.cn>
2019-06-17 16:14:07 +0800

    update

---
+++ static/main.js
@@ -0,0 +1,43 @@
+function doLoad() {
+  	this.canvas = document.getElementById('canvas');
+  	this.cube = null;
+
+  	initWebGL();
+}
+
+function initWebGL() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 45, this.canvas.width / this.canvas.height, 0.1, 1000 );
+
+    var canvas = document.getElementById('canvas');
+    var renderer = new THREE.WebGLRenderer({canvas: canvas});
+    renderer.setSize( canvas.width, canvas.height );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshNormalMaterial();
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    this.cube = cube;
+
+    camera.position.z = 5;
+
+    var render = function () {
+        requestAnimationFrame( render );
+
+        //cube.rotation.x += 0.01;
+        //cube.rotation.y += 0.01;
+        //cube.rotation.z += 0.01;
+
+		pitch=11.52
+  	    roll=14.57
+  	    yaw=-27.32
+
+  	    this.cube.rotation.x = pitch;
+		this.cube.rotation.y = yaw;
+		this.cube.rotation.z = roll;
+
+        renderer.render(scene, camera);
+    };
+
+    render();
+}
