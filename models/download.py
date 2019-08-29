b0288cc671f5edebb93db9c0bb1a887570f004bd
Yang Liu <liuyang12@focusmedia.cn>
2019-08-29 14:33:10 +0800

    update

---
+++ models/download.py
@@ -0,0 +1,18 @@
+from subprocess import Popen, PIPE
+
+from utils import ensure_folder
+
+if __name__ == '__main__':
+    filename = 'download.csv'
+    with open(filename, 'r') as file:
+        data = file.readlines()
+
+    for line in data:
+        folder = line.split(',')[0].strip()
+        address = line.split(',')[1].strip()
+        ensure_folder(folder)
+
+        process = Popen(["wget", address, "-P", folder], stdout=PIPE)
+        (output, err) = process.communicate()
+        exit_code = process.wait()
+        print(output)
