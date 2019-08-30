eb02988591d87c4e9f0cff93189892dd1ccb9e3b
Yang Liu <liuyang12@focusmedia.cn>
2019-08-30 15:35:14 +0800

    update

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-import torch
 import os
+
+import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
 image_w = 112
@@ -11,3 +12,5 @@ STATIC_DIR = 'static'
 UPLOAD_FOLDER = 'upload'
 UPLOAD_DIR = os.path.join(STATIC_DIR, UPLOAD_FOLDER)
 pickle_file = STATIC_DIR + '/' + 'weiweiimage.pkl'
+
+IGNORE_ID = -1
