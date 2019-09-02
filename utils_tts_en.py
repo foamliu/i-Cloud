8c5c945c14b6ad4a49aa2b57ce8ac90595e53590
Yang Liu <liuyang12@focusmedia.cn>
2019-09-02 14:12:57 +0800

    update

--- utils_tts_en.py
+++ utils_tts_en.py
@@ -86,11 +86,12 @@ model.load_state_dict(torch.load(checkpoint))
 model = model.to(device)
 model.eval()
 
-waveglow_path = 'waveglow_256channels.pt'
-waveglow = torch.load(waveglow_path)['model']
-waveglow.cuda().eval().half()
-for k in waveglow.convinv:
-    k.float()
+# waveglow_path = 'waveglow_256channels.pt'
+# waveglow = torch.load(waveglow_path)['model']
+# waveglow.cuda().eval().half()
+# for k in waveglow.convinv:
+#     k.float()
+from utils_tts_cn import waveglow
 
 sampling_rate = 22050
 
