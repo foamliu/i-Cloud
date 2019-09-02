4bdc736e6e5a2dfe42e82a139793fa6316a2ac4c
Yang Liu <liuyang12@focusmedia.cn>
2019-09-02 11:00:50 +0800

    update

--- utils_tts.py
+++ utils_tts.py
@@ -111,9 +111,9 @@ def do_synthesize():
     start = time.time()
     text = request.form['text']
     print('text: ' + str(text))
+    audiopath = synthesize(text)
     elapsed = time.time() - start
     elapsed = float(elapsed)
-    audiopath = synthesize(text)
     return audiopath, elapsed
 
 
