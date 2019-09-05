fd382c87d434aaf4b52d5cd4459df780e63da2c0
Yang Liu <liuyang12@focusmedia.cn>
2019-09-05 09:57:59 +0800

    update

--- utils_tts_cn.py
+++ utils_tts_cn.py
@@ -125,6 +125,7 @@ def clean_text(text):
     text = re.sub('[“”（）《》〈〉]', '', text)
     text = text.replace('1', '一').replace('2', '二').replace('3', '三').replace('4', '四').replace('5', '五')
     text = text.replace('6', '六').replace('7', '七').replace('8', '八').replace('9', '九').replace('0', '零')
+    text = text.replace('.', '点')
     return text
 
 
