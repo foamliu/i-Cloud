b8be896046426dfbd6982a2121342493b6982c74
Yang Liu <liuyang12@focusmedia.cn>
2019-09-27 16:46:20 +0800

    update

--- sdk/test_faces.py
+++ sdk/test_faces.py
@@ -33,6 +33,13 @@ if __name__ == '__main__':
     print(r.status_code)
     print(r.text)
 
+    url = 'http://47.101.196.204:8080/api/v1/faces/expression'
+    print(url)
+    files = {'file': open('test_image_sad.jpg', 'rb')}
+    r = requests.post(url, files=files)
+    print(r.status_code)
+    print(r.text)
+
     url = 'http://47.101.196.204:8080/api/v1/faces/get_feature'
     print(url)
     files = {'file': open('test_image_1.jpg', 'rb')}
